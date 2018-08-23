8d9e3de2e2a31b339027f0e3f5b623174eb00421
Michael Habinsky <mhabinsky@live.com>
2018-08-23 00:30:32 -0400

    finished objects

---
+++ Builders-TestFirst/04-Objects/02-objectifying-arrays/objectifying-arrays.js
@@ -0,0 +1,17 @@
+function objectifier(arr){
+  let obj = {};
+  for(i=0;i<arr.length;i++){
+    let propName = arr[i][0];
+    obj[propName] = {};
+
+    let value = arr[i][1];
+    for(j=0;j<value.length;j++){
+      if(!Array.isArray(value[j])){
+        let valuePropName = value[j];
+        obj[propName][valuePropName] = value[j+1];
+      }
+      
+    }
+  }
+  return obj;
+}
--- Builders-TestFirst/04-Objects/03-frequency-analysis/frequencey-analysis.js
+++ Builders-TestFirst/04-Objects/03-frequency-analysis/frequencey-analysis.js
@@ -1,1 +1,15 @@
+function frequencyAnalysis(Text){
+   let obj = {};
+   for(i=0;i<Text.length;i++){
+     let count = 0;
+     let letter = Text[i];
+     for(j=0;j<Text.length;j++){
+       if(letter === Text[j]){
+         count++;
+       }
+     }
+     obj[letter] = count;
+    }
+    return obj;
+}
 
