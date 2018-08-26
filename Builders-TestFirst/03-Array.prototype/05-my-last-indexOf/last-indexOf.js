0ea33f9283deedb3ee51ce4577e78a202bc29b5e
Michael Habinsky <mhabinsky@live.com>
2018-08-26 14:13:31 -0400

    updated last index of solution

--- Builders-TestFirst/03-Array.prototype/05-my-last-indexOf/last-indexOf.js
+++ Builders-TestFirst/03-Array.prototype/05-my-last-indexOf/last-indexOf.js
@@ -1,12 +1,13 @@
 function myLastIndexOf(arr,value,indexStart){
     let i;
     if(indexStart < 0){
-        i = indexStart + (arr.length-1);
+        i = indexStart + (arr.length);
     }else if(indexStart >= 0){
         i = indexStart;
     }else if(!indexStart){
         i = arr.length-1;
     }
+    console.log("my IndexStart Value: ", i);
     for(i;i>=0;i--){
         if(arr[i] === value){
             return i;
@@ -30,7 +31,7 @@ function myLastIndexOf_solution(arr, searchFor, fromIndex) {
     return -1;
 }
 
-console.log(myLastIndexOf_solution([1,2,3,4,5,6,6,7,8,3,3],6,-1));
-console.log(myLastIndexOf([1, 2, 3, 4, 5, 6, 6, 7, 8, 3, 3], 6, -3));
+console.log("online solution: ", myLastIndexOf_solution([1,2,3,4,5,6,6,7,8,3,3],6,-1));
+console.log("my solution: ",myLastIndexOf([1, 2, 3, 4, 5, 6, 6, 7, 8, 3, 3], 6, -3));
 console.log('hello world!');
 
